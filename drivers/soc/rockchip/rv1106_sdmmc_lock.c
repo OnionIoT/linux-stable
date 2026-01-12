564947b3e31431ee2d044624cf607a08760e8a7c
Zheng Han <zh@onioniot.com>
2026-01-12 12:24:01 -0500

    mipi-csi-support

---
+++ drivers/soc/rockchip/rv1106_sdmmc_lock.c
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/export.h>
+
+void rv1106_sdmmc_get_lock(void)
+{
+}
+EXPORT_SYMBOL_GPL(rv1106_sdmmc_get_lock);
+
+void rv1106_sdmmc_put_lock(void)
+{
+}
+EXPORT_SYMBOL_GPL(rv1106_sdmmc_put_lock);
